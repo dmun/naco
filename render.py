610df4c23c79a12372ad038d0fa3ecaf4a815d71
David van Munster <davidru01@live.nl>
2026-04-25 20:48:03 +0200

    Refactor dataclasses

--- render.py
+++ render.py
@@ -4,32 +4,49 @@ from simulation import Config, State
 CELL_SIZE = 20
 
 
-def draw_agents(state: State, cfg: Config):
-    for i in range(cfg.n_agents):
-        if state.agent_alive[i]:
-            vision = int(state.agent_vision[i])
-            energy = max(
-                0, float(state.agent_energy[i]) / float(state.agent_max_energy[i])
+def iter_agents(state: State):
+    for i in range(len(state.agents.pos)):
+        if state.agents.alive[i]:
+            yield (
+                int(state.agents.pos[i, 0]),
+                int(state.agents.pos[i, 1]),
+                int(state.agents.vision[i]),
+                int(state.agents.energy[i]),
+                int(state.agents.max_energy[i]),
             )
-            x = int(state.agent_pos[i][0]) * CELL_SIZE
-            y = int(state.agent_pos[i][1]) * CELL_SIZE
-            color = rl.Color(255 - int(energy * 255), int(energy * 255), 0, 255)
-            rl.draw_rectangle(
-                x - vision * CELL_SIZE,
-                y - vision * CELL_SIZE,
-                (vision * 2 + 1) * CELL_SIZE,
-                (vision * 2 + 1) * CELL_SIZE,
-                rl.color_alpha(color, 0.1),
-            )
-            rl.draw_rectangle(x, y, CELL_SIZE, CELL_SIZE, color)
 
 
-def draw_food(state: State, cfg: Config):
-    for i in range(cfg.n_food):
-        x = int(state.food_pos[i][0]) * CELL_SIZE
-        y = int(state.food_pos[i][1]) * CELL_SIZE
-        rl.draw_rectangle(x, y, CELL_SIZE, CELL_SIZE, rl.DARKGREEN)
-        rl.draw_rectangle_lines(x, y, CELL_SIZE, CELL_SIZE, rl.BLACK)
+def iter_consumables(state: State):
+    for i in range(len(state.consumables.pos)):
+        yield (
+            int(state.consumables.pos[i, 0]),
+            int(state.consumables.pos[i, 1]),
+            int(state.consumables.energy[i]),
+        )
+
+
+def draw_agents(state: State):
+    for x, y, vision, energy, max_energy in iter_agents(state):
+        energy = max(0, energy / max_energy)
+        color = rl.Color(255 - int(energy * 255), int(energy * 255), 0, 255)
+        rl.draw_rectangle(
+            x * CELL_SIZE - vision * CELL_SIZE,
+            y * CELL_SIZE - vision * CELL_SIZE,
+            (vision * 2 + 1) * CELL_SIZE,
+            (vision * 2 + 1) * CELL_SIZE,
+            rl.color_alpha(color, 0.1),
+        )
+        rl.draw_rectangle(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE, color)
+
+
+def draw_food(state: State):
+    for x, y, energy in iter_consumables(state):
+        rl.draw_rectangle(
+            x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE, rl.DARKGREEN
+        )
+        rl.draw_rectangle_lines(
+            x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE, rl.BLACK
+        )
 
 
 def draw_grid(cfg: Config):
@@ -44,7 +61,7 @@ def draw(state: State, cfg: Config):
     rl.clear_background(rl.DARKGRAY)
 
     draw_grid(cfg)
-    draw_agents(state, cfg)
-    draw_food(state, cfg)
+    draw_agents(state)
+    draw_food(state)
 
     rl.draw_text(f"step: {state.step}", 10, 10, 24, rl.WHITE)
--- simulation.py
+++ simulation.py
@@ -1,3 +1,4 @@
+import jax
 from jax import random, Array
 from dataclasses import dataclass
 import jax.numpy as jnp
@@ -6,99 +7,117 @@ import jax.numpy as jnp
 @dataclass
 class Config:
     n_agents: int = 10
-    n_food: int = 5
-    vision_cost: float = 0.5
+    n_consumables: int = 5
+    vision_cost: float = 0.2
+    metabolism: int = 1
+    food_value: int = 20
     seed: int = 42
     rows: int = 30
     columns: int = 40
 
 
-@dataclass(frozen=True)
+@dataclass
+class Agents:
+    pos: Array
+    energy: Array
+    vision: Array
+    max_energy: Array
+
+    @property
+    def alive(self) -> Array:
+        return self.energy > 0
+
+
+@dataclass
+class Consumables:
+    pos: Array
+    energy: Array
+
+
+@dataclass
 class State:
-    agent_pos: Array
-    agent_energy: Array
-    agent_vision: Array
-    agent_max_energy: Array
-    food_pos: Array
+    agents: Agents
+    consumables: Consumables
     key: Array
     step: int = 0
 
-    @property
-    def agent_alive(self) -> Array:
-        return self.agent_energy > 0
 
-
-def init_state(c: Config) -> State:
-    key = random.key(c.seed)
+def init_state(cfg: Config) -> State:
+    key = random.key(cfg.seed)
     key, k1, k2 = random.split(key, 3)
-    agent_pos = random.randint(
-        k1, shape=(c.n_agents, 2), minval=0, maxval=jnp.array([c.columns, c.rows])
-    )
-    food_pos = random.randint(
-        k2, shape=(c.n_food, 2), minval=0, maxval=jnp.array([c.columns, c.rows])
-    )
 
-    agent_energy = jnp.ones(c.n_agents) * 100
-    agent_vision = random.randint(key, shape=(c.n_agents,), minval=1, maxval=5)
-    agent_max_energy = jnp.ones(c.n_agents) * 100
-
-    return State(
-        agent_pos=agent_pos,
-        agent_energy=agent_energy,
-        agent_vision=agent_vision,
-        agent_max_energy=agent_max_energy,
-        food_pos=food_pos,
-        key=key,
+    agents = Agents(
+        pos=random.randint(
+            k1,
+            shape=(cfg.n_agents, 2),
+            minval=0,
+            maxval=jnp.array([cfg.columns, cfg.rows]),
+        ),
+        energy=jnp.ones(cfg.n_agents) * 100,
+        vision=random.randint(key, shape=(cfg.n_agents,), minval=1, maxval=5),
+        max_energy=jnp.ones(cfg.n_agents) * 100,
     )
 
+    consumables = Consumables(
+        pos=random.randint(
+            k2,
+            shape=(cfg.n_consumables, 2),
+            minval=0,
+            maxval=jnp.array([cfg.columns, cfg.rows]),
+        ),
+        energy=jnp.ones(cfg.n_consumables),
+    )
 
-def respawn(state: State, cfg: Config) -> State:
-    key, k1, k2 = random.split(state.key, 3)
+    return State(agents=agents, consumables=consumables, key=key)
 
-    dead = state.agent_energy <= 0
+
+def respawn(agents: Agents, cfg: Config, key) -> Agents:
+    key, k1, k2 = random.split(key, 3)
+
     parent_idx = random.randint(k1, (cfg.n_agents,), 0, cfg.n_agents)
 
-    new_vision = state.agent_vision[parent_idx] + random.randint(
-        k2, (cfg.n_agents,), -1, 2
-    )
+    new_vision = agents.vision[parent_idx] + random.randint(k2, (cfg.n_agents,), -1, 2)
     new_vision = jnp.where(new_vision > 1, new_vision, 1)
 
     new_pos = random.randint(
         k1, (cfg.n_agents, 2), 0, jnp.array([cfg.columns, cfg.rows])
     )
-    agent_pos = jnp.where(dead[:, None], new_pos, state.agent_pos)
-    agent_vision = jnp.where(dead, new_vision, state.agent_vision)
-    agent_energy = jnp.where(dead, state.agent_max_energy, state.agent_energy)
+    new_pos = jnp.where(~agents.alive[:, None], new_pos, agents.pos)
 
-    return State(
-        agent_pos=agent_pos,
-        agent_energy=agent_energy,
-        agent_vision=agent_vision,
-        agent_max_energy=state.agent_max_energy,
-        food_pos=state.food_pos,
-        key=key,
-        step=state.step,
+    new_vision = jnp.where(~agents.alive, new_vision, agents.vision)
+    new_energy = jnp.where(~agents.alive, agents.max_energy, agents.energy)
+
+    return Agents(
+        pos=new_pos, vision=new_vision, energy=new_energy, max_energy=agents.max_energy
     )
+
+
+def agent_move(state: State, cfg: Config, key) -> Array:
+    agents = state.agents
+    offset = random.randint(key, (cfg.n_agents, 2), -1, 2)
+    return (agents.pos + offset) % jnp.array([cfg.columns, cfg.rows])
 
 
 def step(state: State, cfg: Config) -> State:
-    key, k1, k2, k3 = random.split(state.key, 4)
+    agents = state.agents
+    key, k1, k2 = random.split(state.key, 3)
 
-    if state.agent_alive.sum() / cfg.n_agents < 0.5:
-        state = respawn(state, cfg)
+    if agents.alive.sum() / cfg.n_agents < 0.5:
+        agents = respawn(agents, cfg, k1)
 
-    offset = random.randint(k1, shape=(cfg.n_agents, 2), minval=-1, maxval=2)
-    new_agent_pos = (state.agent_pos + offset) % jnp.array([cfg.columns, cfg.rows])
     new_agent_energy = (
-        state.agent_energy - 2 - (cfg.vision_cost * (state.agent_vision ^ 2))
+        agents.energy - cfg.metabolism - (cfg.vision_cost * agents.vision**2)
     )
+    new_agent_pos = agent_move(state, cfg, k2)
 
     return State(
-        agent_pos=new_agent_pos,
-        agent_energy=new_agent_energy,
-        agent_vision=state.agent_vision,
-        agent_max_energy=state.agent_max_energy,
-        food_pos=state.food_pos,
+        agents=Agents(
+            new_agent_pos,
+            new_agent_energy,
+            agents.vision,
+            agents.max_energy,
+        ),
+        consumables=state.consumables,
         key=key,
         step=state.step + 1,
     )
